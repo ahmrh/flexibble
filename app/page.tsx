8ca66e8ba2601ca6b8e3a2ecdb7716494f9f9a37
ahmrh <ahmadrolandi01@gmail.com>
2023-08-31 14:06:34 +0700

    Adding base grafbase project

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 const Home = () => {
     return (
-        <section className="flex-start"></section>
+        <section className="flex-start flex-col paddings mb-16">
+            <h1>Categories</h1>
+            <h1>Posts</h1>
+            <h1>LoadMore</h1>
+        </section>
     )
 }
 
